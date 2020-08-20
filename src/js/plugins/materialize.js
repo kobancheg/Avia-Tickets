4ff062a0245de2ab82fdc30e502aa5bcad9554fd
kobancheg <aleks.revati@gmail.com>
2020-08-20 19:06:05 +0300

    template forms

--- src/js/plugins/materialize.js
+++ src/js/plugins/materialize.js
@@ -3,9 +3,33 @@ import 'materialize-css/dist/js/materialize.min.js'
 
 
 // Init select
-const select =  document.querySelectorAll('select');
+const select = document.querySelectorAll('select');
 M.FormSelect.init(select);
 
 export function getSelectInstance(elem) {
   return M.FormSelect.getInstance(elem);
+}
+
+// Init autocomplit
+const autocomplete = document.querySelectorAll('.autocomplete');
+M.Autocomplete.init(autocomplete, {
+  data: {
+    "Apple": null,
+    "Microsoft": null,
+    "Google": 'https://placehold.it/250x250'
+  }
+});
+
+export function getAutocompleteInstance(elem) {
+  return M.Autocomplete.getInstance(elem);
+}
+
+// Init Datepicker
+const datepicker = document.querySelectorAll('.datepicker');
+M.Datepicker.init(datepicker, {
+  showClearBtn: true
+});
+
+export function getDatepickerInstance(elem) {
+  return M.Datepicker.getInstance(elem);
 }
