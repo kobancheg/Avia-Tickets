68f97a48ac2495e142c2d4394a6ec01de458d88e
kobancheg <aleks.revati@gmail.com>
2020-08-22 19:30:41 +0300

    get & set currency

--- src/js/app.js
+++ src/js/app.js
@@ -2,6 +2,7 @@ import '../css/style.css';
 import './plugins';
 import locations from './store/locations';
 import formUI from './views/form';
+import currencyUI from './views/currency';
 
 document.addEventListener('DOMContentLoaded', () => {
   initApp();
@@ -25,13 +26,15 @@ document.addEventListener('DOMContentLoaded', () => {
     const destination = locations.getCityCodeByKey(formUI.destinationValue);
     const depart_date = formUI.departDateValue;
     const return_date = formUI.returnDateValue;
+    const currency = currencyUI.currencyValue;
     // CODE, CODE, 2019-09, 2019-10
     console.log(origin, destination, depart_date, return_date);
     await locations.fetchTickets({
       origin,
       destination,
       depart_date,
-      return_date
+      return_date,
+      currency
     })
   }
 
