0a1bbdb1c0846dc2121874e5def615a193fb5e41
kobancheg <aleks.revati@gmail.com>
2020-08-23 10:59:12 +0300

    get airlines icons

--- src/js/store/locations.js
+++ src/js/store/locations.js
@@ -6,19 +6,22 @@ class Locations {
     this.countries = null;
     this.cities = null;
     this.shortCitiesList = null;
+    this.airlines = {};
   }
 
   async init() {
     const response = await Promise.all([
       this.api.countries(),
-      this.api.cities()
+      this.api.cities(),
+      this.api.airlines()
     ])
 
-    const [countries, cities] = response;
+    const [countries, cities, airlines] = response;
     this.countries = this.serializeCountries(countries);
     this.cities = this.serializeCities(cities);
     this.shortCitiesList = this.createShortCitiesList(this.cities);
-
+    this.airlines = this.serializeAirlines(airlines);
+    console.log(this.airlines);
     return response;
   }
 
@@ -33,6 +36,15 @@ class Locations {
       acc[key] = null;
       return acc;
     }, {});
+  }
+
+  serializeAirlines(airlines) {
+    return airlines.reduce((acc, item) => {
+      item.logo = `http://pics.avs.io/200/200/${item.code}.png`;
+      item.name = item.name || item.name_translations.en;
+      acc[item.code] = item;
+      return acc;
+    }, {})
   }
 
   serializeCountries(countries) {
